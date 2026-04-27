5bb98ff96b0a74a2020dd9bea7d91880254a22bc
amritDS-beginner <amrit.nt.9@gmail.com>
2026-04-27 10:24:31 +0545

    Initial commit with tests

---
+++ tests/math_utils_test.py
@@ -0,0 +1,11 @@
+from math_utils import add, divide
+import pytest
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_divide():
+    assert divide(10, 2) == 5
+    
+def test_fail_case():
+    assert add(2, 2) == 5
